bc3ab26828521bee13946bff983d121ddbc473de
Pedro Augusto <alfap97augusto@gmail.com>
2018-11-25 17:09:36 -0200

    vai carai

--- TrabalhoFinal [Redes]/src/br/quixada/ufc/rsd/Server/Cliente.java
+++ TrabalhoFinal [Redes]/src/br/quixada/ufc/rsd/Server/Cliente.java
@@ -1,5 +1,33 @@
 package br.quixada.ufc.rsd.Server;
 
 public class Cliente {
+	
+	private static int id_cliente;
+	private String nome;
+	private String telefone; 
+	
+	public Cliente(String nome, String telefone) {
+		this.nome = nome;
+		this.telefone = telefone;
+	}
 
+	public static int getId_cliente() {
+		return id_cliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 }
