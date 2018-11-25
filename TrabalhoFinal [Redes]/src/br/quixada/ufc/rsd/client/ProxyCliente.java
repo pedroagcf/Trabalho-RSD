3ae3bd74b75a7b675aa42f33cca69231b3c5ffa0
Pedro Augusto <alfap97augusto@gmail.com>
2018-11-25 16:11:31 -0200

    classe mototaxi criada

--- TrabalhoFinal [Redes]/src/br/quixada/ufc/rsd/client/ProxyCliente.java
+++ TrabalhoFinal [Redes]/src/br/quixada/ufc/rsd/client/ProxyCliente.java
@@ -12,32 +12,32 @@ public class ProxyCliente  {
     }
     
     public boolean cadastrarCliente(String nome, String telefone, String praca) throws IOException{
-        tcp.sendRequest();        
+        tcp.sendRequest("Calculadora,add,"+op1+","+op2);        
         return Double.parseDouble(tcp.getResponse());
     }
     
     public ArrayList<MotoTaxi> listarCliente() throws IOException{
-        tcp.sendRequest();
+        tcp.sendRequest("Calculadora,sub,"+op1+","+op2);
         return Double.parseDouble(tcp.getResponse());
     }
     
     public boolean editarCliente(String nome, String telefone, String praca) throws IOException{
-        tcp.sendRequest();        
+        tcp.sendRequest("Calculadora,mult,"+op1+","+op2);        
         return Double.parseDouble(tcp.getResponse());
     }
     
     public boolean loginCliente(int idMotoTaxi) throws IOException{
-        tcp.sendRequest();
+        tcp.sendRequest("Calculadora,div,"+op1+","+op2);
         return Double.parseDouble(tcp.getResponse());
     }
     
     public boolean deslogar() throws IOException{
-        tcp.sendRequest();
+        tcp.sendRequest("Calculadora,div,"+op1+","+op2);
         return Double.parseDouble(tcp.getResponse());
     }
     
     public void close() throws IOException{
-        tcp.sendRequest();
+        tcp.sendRequest("Calculadora,div,"+op1+","+op2);
     }
     
     
